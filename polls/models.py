0ddff31c903eda5f74213bceebf32f2b2c302925
ashok3795 <ashok.sharma3795@gmail.com>
2019-08-28 18:25:07 +0545

    vayooo

--- polls/models.py
+++ polls/models.py
@@ -8,8 +8,11 @@ from django.utils import timezone
 
 class Question(models.Model):
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-     # ...
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     def __str__(self):
         return self.question_text
@@ -23,5 +26,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
+    
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
 # Create your models here.
